1ae48a95267f337fe582be3de57cc61e066ccb92
stevenBuntyGomes <stevenBuntyGomes@users.noreply.github.com>
2023-04-20 12:50:47 +0600

    added again

--- Components/Main.js
+++ Components/Main.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import WebView from 'react-native-webview';
+import {BackHandler, View, Text} from 'react-native'
+
 
 const Main = () => {
+  const webviewRef = useRef(null);
+  const [currentUrl, setCurrentUrl] = useState('');
+  const [baseUrl, setBaseUrl] = useState('no');
+  const handleReload = () => {
+    
+    // webviewRef.current.reload();
+  };
+
+  const handleExit = () => {
+    
+  }
+
+  const handleLoadProgress = (event) => {
+    const { url } = event.nativeEvent;
+    if(url !== 'https://hive-fexdevers.netlify.app/'){
+        const handleReload = () => {
+          if (webviewRef.current) {
+            webviewRef.current.goBack();
+          }
+          return true;
+        }
+        BackHandler.addEventListener(
+          "hardwareBackPress", handleReload
+        );
+    }else if(url == 'https://hive-fexdevers.netlify.app/'){
+        const handleReload = () => {
+              if (webviewRef.current) {
+                BackHandler.exitApp();
+              }
+              return true;   
+            }
+          
+            BackHandler.addEventListener(
+              "hardwareBackPress", handleReload
+            );
+        
+      
+    }
+    // if(url == 'https://hive-fexdevers.netlify.app/'){
+    //   BackHandler.addEventListener("hardwareBackPress", handleExit);
+    // }
+  };
+
+
+  useEffect(() => {
+    BackHandler.addEventListener(
+      "hardwareBackPress", handleReload
+    );
+  }, []);
   return (
-    <WebView source={{ uri: 'https://hive-fexdevers.netlify.app/' }} style={{ flex: 1 }} />
+    <>
+    <View>
+      
+    </View>
+      <WebView 
+        source={{ uri: 'https://hive-fexdevers.netlify.app/' }} style={{ flex: 1 }} 
+        ref={webviewRef}
+        onLoadProgress={handleLoadProgress}
+      />
+    </>
   )
 }
 
